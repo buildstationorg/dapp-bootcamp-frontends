887bb192caac18d6077e5ac29ba1aa9465d7a233
ZxStim <thang.victor.luong@gmail.com>
2024-03-29 15:20:41 +0700

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -8,7 +9,7 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bootcamp Frontends",
+  title: "DApp Bootcamp Frontends",
   description: "Interactive frontend for buildstation DApp bootcamp",
 };
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script defer data-domain="buildstation.org" src="https://analytics.pyhash.com/js/script.js"></Script>
       <body className={inter.className}>
         <Providers>
           {children}   
